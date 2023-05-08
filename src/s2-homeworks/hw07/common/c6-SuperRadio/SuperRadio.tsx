ac98f63125f67917decd4680c9c04baf154f5546
SchoolboyKreator <dmitryshtef@gmail.com>
2023-05-08 22:44:56 +0700

    hw7 done without styles

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -58,7 +58,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                         {...restProps}
                     />
                     <span
-                        id={id + '-span-' + o.id}
+                        id={id + '-span-' + o.value}
                         {...spanProps}
                         className={spanClassName}
                     >
