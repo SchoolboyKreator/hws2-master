2943c6982685c9df608a8b2a9053f4ff2199d139
SchoolboyKreator <dmitryshtef@gmail.com>
2023-05-09 19:56:58 +0700

    hw8 I think it's working ppc

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,15 +7,11 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            const newState = [...state].sort((a,b) => {
-                if (a.name > b.name) return 1
-                else if (a.name > b.name) return  -1
-                else return 0
-            })
-            return  action.payload === 'up' ? newState : newState.reverse()
+            const newState = [...state].sort((a, b) => a.name.localeCompare(b.name));
+            return action.payload === 'up' ? newState : newState.reverse()
         }
         case 'check': {
-            return state.filter(user => user.age > action.payload) // need to fix
+            return state.filter(user => user.age > action.payload).sort((a, b) => a.name.localeCompare(b.name)) // need to fix
         }
         default:
             return state
